fb95f094a53d9e3cc073c0057b3ab2a759965856
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-12 15:40:33 -0300

    Criando mascara global para valor

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -20,12 +20,12 @@ module.exports = {
       }
     }
 
-    const results = await Product.create(req.body);
+    let results = await Product.create(req.body);
     const productId = results.rows[0].id;
     results = await Category.all();
     const categories = results.rows
 
-    return res.render(`/products/create`, { productId, categories })
+    return res.render(`products/create`, { productId, categories })
 
   },
 
