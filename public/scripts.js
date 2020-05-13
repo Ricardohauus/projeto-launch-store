c5bddc78b9a9a63a21f80b873017c5f6d1134e0b
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-13 13:57:33 -0300

    Estilizando o file e inserindo limitações

--- public/scripts.js
+++ public/scripts.js
@@ -12,3 +12,43 @@ const Mask = {
     }).format(value / 100)
   }
 }
+
+const PhotosUpload = {
+  uploadLimit: 6,
+  handleFileInput(event) {
+    const { files: fileList } = event.target;
+
+    if (this.hasLimit()) return
+
+    Array.from(fileList).forEach(file => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const image = new Image();
+        image.src = String(reader.result)
+        const div = PhotosUpload.getContainer(image);
+        document.querySelector("#photos-preview").appendChild(div);
+      }
+      reader.readAsDataURL(file);
+
+    })
+
+  },
+  getContainer(image) {
+    const div = document.createElement('div');
+    div.classList.add('photo');
+    div.onclick = () => alert("Remover foto");
+    div.appendChild(image);
+    return div;
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload;
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`);
+      event.preventDefault();
+      return true;
+    }
+    return false;
+  }
+
+
+}
