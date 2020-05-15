7226c3d9a9ca14c9f027683b342018273cf71216
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-15 13:48:10 -0300

    Criando o macro para o search

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const ProductController = require("./app/controllers/ProductController")
 const HomeController = require("./app/controllers/HomeController")
+const SearchController = require("./app/controllers/SearchController")
 const multer = require("./app/middlewares/multer")
 
+//Index
 routes.get("/", HomeController.index)
 
+
+//Search
+routes.get("/products/search", SearchController.index)
+
+//Products
 routes.get("/products/create", ProductController.create)
 routes.get("/products/:id/edit", ProductController.edit)
 routes.get("/products/:id", ProductController.show)
@@ -15,9 +22,11 @@ routes.put("/products", multer.array("photos", 6), ProductController.saveOrUpdat
 routes.delete("/products", ProductController.delete)
 
 
+//Alias
 routes.get("/ads/create", function (req, res) {
   res.redirect("/products/create")
 })
+
 
 
 module.exports = routes;
