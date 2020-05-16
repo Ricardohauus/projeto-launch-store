11459c42e3b69e885f251801eb9e01586618a12d
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-16 14:23:14 -0300

    Adicionado a mascara para CPF-CNPJ E CEP

--- public/scripts.js
+++ public/scripts.js
@@ -10,6 +10,31 @@ const Mask = {
       style: 'currency',
       currency: 'BRL'
     }).format(value / 100)
+  },
+  cpfCnpj(value) {
+    value = value.replace(/\D/g, "");
+    if (value.length == 11) {
+      // CPF - 123.456.789-11
+      value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+    } else if (value.length == 14) {
+      // CNPJ - 12.345.678/9000-14
+      value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+    } else if (value.length > 14) {
+      value = value.substring(0, 14);
+      value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+    }
+    return value;
+  },
+  cep(value) {
+    value = value.replace(/\D/g, "");
+    if (value.length == 8) {
+      // CEP 12345-678
+      value = value.replace(/(\d{5})(\d{3})/, "$1-$2")
+    } else if (value.length > 8) {
+      value = value.substring(0, 8);
+      value = value.replace(/(\d{5})(\d{3})/, "$1-$2")
+    }
+    return value;
   }
 }
 
